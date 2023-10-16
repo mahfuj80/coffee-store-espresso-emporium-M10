e850212eca68f44d723f8ef3b5f783a7269c42f0
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-16 22:33:04 +0600

    error page created

--- coffee-store-espresso-emporium-client/src/Pages/ErrorPage.jsx
+++ coffee-store-espresso-emporium-client/src/Pages/ErrorPage.jsx
@@ -1,7 +1,20 @@
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+
 const ErrorPage = () => {
   return (
     <div>
-      <h2>This is Error Page</h2>
+      <Link>
+        <div className="flex items-center w-fit py-1 px-2 mt-6 gap-3 bg-slate-200 hover:bg-slate-300 rounded lg: mx-auto md:ml-20 ">
+          <FaArrowAltCircleLeft></FaArrowAltCircleLeft>
+          <p className="text-3xl">Back to home</p>
+        </div>
+      </Link>
+      <div className="w-full">
+        <img className="mx-auto" src="/images/404/404.gif" alt="Error_Image" />
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
